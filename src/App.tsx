2bb04f9fbb125ef58e58932904fec3c73c9de235
mike-holm-percipient <mike.holm@percipient.ai>
2025-03-14 19:05:32 -0700

    add sort order

--- src/App.tsx
+++ src/App.tsx
@@ -52,8 +52,18 @@ const aggregatedTrips: AggregatedTrip[] = trips.map((trip) => ({
 
 function App() {
   const [sortField, setSortField] = useState<keyof AggregatedTrip>('travelers');
+  const [isAscending, setIsAscending] = useState<boolean>(true);
 
-  const sortedTrips = useMemo(() => aggregatedTrips.sort((a, b) => (a[sortField] > b[sortField] ? 1 : -1)), [trips, sortField]);
+  const sortedTrips = useMemo(() => aggregatedTrips.sort((a, b) => (a[sortField] > b[sortField] ? 1 : -1) * (isAscending ? 1 : -1)), [trips, sortField, isAscending]);
+
+  const handleSort = (field: keyof AggregatedTrip) => {
+    if (field === sortField) {
+      setIsAscending(!isAscending);
+    } else {
+      setSortField(field);
+      setIsAscending(true);
+    }
+  }
 
   return (
     <>
@@ -61,20 +71,18 @@ function App() {
       <table>
         <thead>
         <tr>
-          <th>Id</th>
-          <th onClick={() => setSortField('destination')}>Destination</th>
-          <th onClick={() => setSortField('startDate')}>Start Date</th>
-          <th onClick={() => setSortField('endDate')}>End Date</th>
-          <th onClick={() => setSortField('adults')}>Adults</th>
-          <th onClick={() => setSortField('children')}>Children</th>
-          <th onClick={() => setSortField('travelers')}>Travelers</th>
-          <th onClick={() => setSortField('nights')}>Nights</th>
+          <th onClick={() => handleSort('destination')}>Destination</th>
+          <th onClick={() => handleSort('startDate')}>Start Date</th>
+          <th onClick={() => handleSort('endDate')}>End Date</th>
+          <th onClick={() => handleSort('adults')}>Adults</th>
+          <th onClick={() => handleSort('children')}>Children</th>
+          <th onClick={() => handleSort('travelers')}>Travelers</th>
+          <th onClick={() => handleSort('nights')}>Nights</th>
         </tr>
         </thead>
         <tbody>
         {sortedTrips.map((trip) => (
           <tr key={trip.id}>
-            <td>{trip.id}</td>
             <td>{trip.destination}</td>
             <td>{trip.startDate.toLocaleDateString()}</td>
             <td>{trip.endDate.toLocaleDateString()}</td>
