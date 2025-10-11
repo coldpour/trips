c133cce52f69c50db31a3212952db634a323e60e
mike-holm-percipient <mike.holm@percipient.ai>
2025-10-11 15:03:57 -0700

    paste from supa base docs

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './index.css'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient('https://tnyckutfhrdjqqhixswv.supabase.co', 'sb_publishable_qm1auNuQQuk7saokdh5lTw_I4aU5AU3')
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
+}
