103e6fc9c57bffd64628bedebf153eb00bb57ad8
mike-holm-percipient <mike.holm@percipient.ai>
2025-10-12 22:28:34 -0700

    reorder the create form

--- src/CreateTrip.tsx
+++ src/CreateTrip.tsx
@@ -64,9 +64,7 @@ function TripDetails() {
   const [childcare, setChildcare] = useState(0);
   const [lodgingTotal, setLodgingTotal] = useState(0);
   const [lodgingPerNight, setLodgingPerNight] = useState(0);
-  const [lodgingPerPersonPerNight, setLodgingPerPersonPerNight] = useState(
-    0,
-  );
+  const [lodgingPerPersonPerNight, setLodgingPerPersonPerNight] = useState(0);
 
   const props: PendingTrip = {
     name,
@@ -84,38 +82,101 @@ function TripDetails() {
     lodgingTotal,
     lodgingPerNight,
     lodgingPerPersonPerNight,
-  }
+  };
 
   const nightsValue = nights || calcNights(props);
 
   return (
     <form onSubmit={handleSubmit}>
-      <h3>Score: {calcScore(props)}</h3>
-      <Input name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <Input name="fun" value={fun} onChange={(e) => setFun(Number(e.target.value))}/>
-      <h3>Cost: {formatCurrency(expenseTotal(props))}</h3>
-
-      <Input name="nights" value={nightsValue} onChange={(e) => setNights(Number(e.target.value))}/>
-      <Input name="arrive" type="date" value={arrive} onChange={(e) => setArrive(e.target.value)}/>
-      <Input name="depart" type="date" value={depart} onChange={(e) => setDepart(e.target.value)}/>
+      <Input
+        name="name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <Input
+        name="nights"
+        value={nightsValue}
+        onChange={(e) => setNights(Number(e.target.value))}
+      />
+      <Input
+        name="arrive"
+        type="date"
+        value={arrive}
+        onChange={(e) => setArrive(e.target.value)}
+      />
+      <Input
+        name="depart"
+        type="date"
+        value={depart}
+        onChange={(e) => setDepart(e.target.value)}
+      />
       <h3>People: {calcTravelers(props)}</h3>
-      <Input name="adults" value={adults} onChange={(e) => setAdults(Number(e.target.value))}/>
-      <Input name="children" value={children} onChange={(e) => setChildren(Number(e.target.value))}/>
+      <Input
+        name="adults"
+        value={adults}
+        onChange={(e) => setAdults(Number(e.target.value))}
+      />
+      <Input
+        name="children"
+        value={children}
+        onChange={(e) => setChildren(Number(e.target.value))}
+      />
 
       <h3>Travel: {formatCurrency(calcTravel(props))}</h3>
-      <Input name="flightCostPerSeat" value={flightCostPerSeat} onChange={(e) => setFlightCostPerSeat(Number(e.target.value))} />
-      <Input name="taxiOrRentalCar" value={taxiOrRentalCar} onChange={(e) => setTaxiOrRentalCar(Number(e.target.value))} />
+      <Input
+        name="flightCostPerSeat"
+        value={flightCostPerSeat}
+        onChange={(e) => setFlightCostPerSeat(Number(e.target.value))}
+      />
+      <Input
+        name="taxiOrRentalCar"
+        value={taxiOrRentalCar}
+        onChange={(e) => setTaxiOrRentalCar(Number(e.target.value))}
+      />
 
       <h3>Lodging: {formatCurrency(calcLodgingTotal(props))}</h3>
-      <Input name="lodgingTotal" value={lodgingTotal} onChange={(e) => setLodgingTotal(Number(e.target.value))} />
-      <Input name="lodgingPerNight" value={lodgingPerNight} onChange={(e) => setLodgingPerNight(Number(e.target.value))} />
-      <Input name="lodgingPerPersonPerNight" value={lodgingPerPersonPerNight} onChange={(e) => setLodgingPerPersonPerNight(Number(e.target.value))} />
+      <Input
+        name="lodgingTotal"
+        value={lodgingTotal}
+        onChange={(e) => setLodgingTotal(Number(e.target.value))}
+      />
+      <Input
+        name="lodgingPerNight"
+        value={lodgingPerNight}
+        onChange={(e) => setLodgingPerNight(Number(e.target.value))}
+      />
+      <Input
+        name="lodgingPerPersonPerNight"
+        value={lodgingPerPersonPerNight}
+        onChange={(e) => setLodgingPerPersonPerNight(Number(e.target.value))}
+      />
 
       <h3>Expenses: {formatCurrency(calcOtherExpenses(props))}</h3>
-      <Input name="entertainment" value={entertainment} onChange={(e) => setEntertainment(Number(e.target.value))}/>
-      <Input name="skiPassPerDay" value={skiPassPerDay} onChange={(e) => setSkiPassPerDay(Number(e.target.value))} />
-      <Input name="childcare" value={childcare} onChange={(e) => setChildcare(Number(e.target.value))}/>
+      <Input
+        name="entertainment"
+        value={entertainment}
+        onChange={(e) => setEntertainment(Number(e.target.value))}
+      />
+      <Input
+        name="skiPassPerDay"
+        value={skiPassPerDay}
+        onChange={(e) => setSkiPassPerDay(Number(e.target.value))}
+      />
+      <Input
+        name="childcare"
+        value={childcare}
+        onChange={(e) => setChildcare(Number(e.target.value))}
+      />
 
+      <h3>Cost: {formatCurrency(expenseTotal(props))}</h3>
+      <Input
+        name="fun"
+        value={fun}
+        onChange={(e) => setFun(Number(e.target.value))}
+      />
+      <h3>Score: {calcScore(props)}</h3>
       <button type="submit" disabled={isPending}>
         Save
       </button>
