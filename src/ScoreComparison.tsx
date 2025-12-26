0262b338e20440b6ff1e050368630bcaf2f7ffe5
coldpour <coldpour@gmail.com>
2025-12-26 13:38:08 -0500

    Vertically center score line

--- src/ScoreComparison.tsx
+++ src/ScoreComparison.tsx
@@ -79,7 +79,7 @@ export function ScoreComparison({
       <div className="calculated-value highlight" style={{ fontSize: '24px', marginBottom: 'var(--space-lg)' }}>
         Trip Score: {currentScore}
       </div>
-      <div style={{ position: "relative", padding: "40px 0" }}>
+      <div style={{ position: "relative", padding: "40px 0", display: "flex", alignItems: "center" }}>
         {/* Number line */}
         <div 
           style={{
